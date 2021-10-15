bb36ae527792d702bdfd7797872124b0a6abb665
Brett Seifried <seifriedbrett@gmail.com>
2021-10-15 13:54:22 -0700

    fixed styling to work! @ 1:54

--- render-food.js
+++ render-food.js
@@ -1,11 +1,15 @@
 export function renderFood(food){
-    const foodCard = document.createElement('div');
-    foodCard.classList.add = 'food-list';
+    const foodCard = document.createElement('li');
+    foodCard.classList.add('food-item');
+
+    // const foodSpan = document.createElement('span');
+    // foodSpan.classList.add = 'food-name';
 
     const img = document.createElement('img');
     img.src = food.img;
 
     const foodName = document.createElement('span');
+    foodName.classList.add('food-name');
     foodName.textContent = food.name;
 
     foodCard.append(img, foodName);
